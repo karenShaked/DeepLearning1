adding and fixing Jacobian test

JacobianTest.py
@@ -15,43 +15,51 @@ class JacTest:
         activation = Activation(activation_name)
         return lambda w: activation.apply(np.dot(w, input_value) + biases)
 
-    @staticmethod
-    def jac_m_v(jac_vec, v):
-        return np.multiply(jac_vec, v)
-
-    def __init__(self, func, input_val):
+    def __init__(self, func, input_val, val_name):
+        self.val_name = val_name
         self.eps0 = 1
         self.eps_i = 0.5
         self.func = func
         self.col, self.row = input_val.shape[0], input_val.shape[1]
-        random_vec = np.random.rand(self.col * self.row)
+        random_vec = np.random.rand(self.col, self.row)
         norm = np.linalg.norm(random_vec)
         self.d_vec = random_vec / norm
         self.input = input_val
+
+    def jac_m_v(self, jac_val, v):
+        result = None
+        if self.val_name == "x":
+            # x -> (input_dim, 1)
+            # v = d -> (input_dim, 1)
+            # jac_val -> (output_dim, input_dim)
+            result = np.dot(jac_val, v)
+        elif self.val_name == "w":
+            # w -> (output-dim, input_dim)
+            # v = d -> (output-dim, input_dim)
+            # jac_val -> (output_dim, input_dim)
+            result = np.sum(v * jac_val, axis=1).reshape(self.col, 1)
+        return result
 
     def jacobian_test(self, iterations, jac_input):
         O_e = []
         O_e2 = []
         for i in range(iterations):
             f_x = self.func(self.input)
-            x_eps_d = self.input + (self.eps0 * self.eps_i ** i) * self.d_vec.reshape(self.col, self.row)
+            x_eps_d = self.input + (self.eps0 * self.eps_i ** i) * self.d_vec
             f_x_eps_d = self.func(x_eps_d)
-            eps_d_transpose = np.transpose((self.eps0 * self.eps_i ** i) * self.d_vec)
-            O_e.append(np.linalg.norm(f_x - f_x_eps_d))
-            O_e2.append(np.linalg.norm(f_x_eps_d - f_x - self.jac_m_v(jac_input, eps_d_transpose)))
+            eps_d = (self.eps0 * self.eps_i ** i) * self.d_vec
+            O_e.append(np.log(np.linalg.norm(f_x - f_x_eps_d)))
+            O_e2.append(np.log(np.linalg.norm(f_x_eps_d - f_x - self.jac_m_v(jac_input, eps_d))))
         self.plot_jac(O_e, O_e2)
 
     def plot_jac(self, O_e, O_e2):
         indexes = range(1, len(O_e) + 1)
-
         plt.figure(figsize=(10, 5))
-
         plt.plot(indexes, O_e, marker='o', linestyle='-', color='blue', label='O_e1')
         plt.plot(indexes, O_e2, marker='s', linestyle='-', color='red', label='O_e2')
-
         plt.xlabel('Index')
         plt.ylabel('Value')
-        plt.title('O(eps) and O(eps^2) Values by Index')
+        plt.title(f"Jacobian of {self.val_name} - O(eps) and O(eps^2) Values by Index")
         plt.legend()
         plt.grid(True)
         plt.show()

Layer.py
@@ -35,33 +35,53 @@ class Layer:
         self.input = input_matrix
         self.batch_size = input_matrix.shape[0]
         self.z = self.calc_wxb(self.input)
+        # w dims -> (output_dim, input_dim)
+        # x dims -> (input_dim, batch_size)
+        # b dims -> (output_dim, 1)
+        # z = wx+b ->(output_dim, batch_size)
         self.layer_result = self.layer_func(self.input)
+        # layer_result = activation(z) ->(output, batch_size)
         return self.layer_result
 
-    def backward(self, next_layers_gradient, jac_test=False):
-        """
-        :param next_layers_gradient: (output_dim * batch_size) gradient from next layers
-        :return: gradient of x of this layer - (input_dim * batch_size)
-                 gradient of theta of this layer
-                 grad_f_w - (batch_size * input_dim * output_dim) * 1
-                 grad_f_b - (batch_size * output_dim * 1) * 1
-                 grad_f_theta - (output_dim * ( 1 + input_dim)) * 1
-        """
-        layer_j = self.layer_func_derivative(self.z)
-        grad_f_b = np.multiply(layer_j, next_layers_gradient)
-        grad_f_w = np.dot(grad_f_b, np.transpose(self.input))
-        grad_f_x_i = np.dot(np.transpose(self.weights), grad_f_b)
-        grad_f_b = grad_f_b.sum(axis=1).reshape(-1, 1)
+    def backward(self, next_layers_gradient, jac_test):
+        activation_derivative = self.layer_func_derivative(self.z)
+        # activation_derivative = derivative_activation(z) ->(output_dim, batch_size)
+        grad_f = np.multiply(activation_derivative, next_layers_gradient)
+        # grad_f = layer_j * next_layers_gradient ->(output_dim, batch_size)
+        grad_f_w = np.dot(grad_f, np.transpose(self.input))
+        # grad_f_w = grad_f @ x.T ->(output_dim, batch_size)*(batch_size, input_dim)->(output_dim, input_dim)
+        grad_f_x_i = np.dot(np.transpose(self.weights), grad_f)
+        # grad_f_x_i = w.T @ grad_f ->(input_dim, output_dim)*(output, batch_size)->(input, batch_size)
+        grad_f_b = grad_f.sum(axis=1).reshape(-1, 1)
+        # grad_f_b = sum over columns(grad_f) ->(output, 1)
         grad_f_theta = np.concatenate((grad_f_w.flatten(), grad_f_b.flatten()), axis=0)
         original_theta = np.concatenate((self.weights.flatten(), self.biases.flatten()))
         if jac_test:
-            self.jac_tests_w_x(grad_f_w, grad_f_x_i)
+            # only for batch_size = 1
+            input_one_col = self.input[:, 0:1]
+            # input_one_col -> (input_dim, 1)
+            activation_derivative_one_col = activation_derivative[:, 0:1]
+            # activation_derivative_one_col -> (output_dim, 1)
+            jac_w = np.outer(activation_derivative_one_col, input_one_col)
+            # [[σ'(z1) * x1, σ'(z1) * x2,...],
+            # ....
+            # [σ'(zm) * x1,... ]]
+            # jac_w -> (output_dim, input_dim)
+            # the real jacobian doesn't look exactly like this (it is padded with many zeros),
+            # but we made the proper changes to make it right and improve performance
+            jac_x = np.multiply(self.weights, np.dot(activation_derivative_one_col, np.ones((1, self.input_dim))))
+            # [[σ'(z1) * w11, σ'(z1) * w12 ,...],
+            # ...
+            # [σ'(zm) * wm1, ... ]]
+            # jac_x -> (output_dim, input_dim)
+            self.jac_tests_w_x(jac_w, jac_x, input_one_col)
+
         return grad_f_x_i, grad_f_theta, original_theta
 
-    def jac_tests_w_x(self, grad_w, grad_x):
+    def jac_tests_w_x(self, grad_w, grad_x, input_x):
         from JacobianTest import JacTest
-        test_grad_w = JacTest(JacTest.func_by_w(self.activation_name, self.input, self.biases), self.weights)
-        test_grad_x = JacTest(JacTest.func_by_x(self.activation_name, self.weights, self.biases), self.input)
+        test_grad_w = JacTest(JacTest.func_by_w(self.activation_name, input_x, self.biases), self.weights, "w")
+        test_grad_x = JacTest(JacTest.func_by_x(self.activation_name, self.weights, self.biases), input_x, "x")
         i = 10
         test_grad_w.jacobian_test(i, grad_w)
         test_grad_x.jacobian_test(i, grad_x)
@@ -78,6 +98,3 @@ class Layer:
         self.weights = updated_weights.reshape(self.weights.shape)
         self.biases = updated_biases.reshape(self.biases.shape)
         return remaining_params
-
-    def grad_tests_w_x_b(self, grad_w, grad_b, grad_x):
-        pass

NeuralNetwork.py
@@ -15,7 +15,7 @@ class NeuralNetwork:
         input_dim = data_dimension
         output_dim = data_dimension
         for i in range(layer_len):
-            output_dim = data_dimension * 16
+            output_dim = data_dimension * 5
             self.layers.append(Layer(hidden_activation, input_dim, output_dim))
             input_dim = output_dim
         self.loss_layer = Loss(loss_name, final_activation, output_dim, labels_len)
@@ -40,26 +40,26 @@ class NeuralNetwork:
         success_percentage = (success_count / self.batch_size) * 100
         return success_percentage
 
-    def backpropagation(self, d_theta, original_theta_loss, dx_loss, grad_test):
+    def backpropagation(self, d_theta, original_theta_loss, dx_loss, jac_test):
         all_theta_grad = d_theta
         all_theta_orig = original_theta_loss
         next_layers_gradient = dx_loss
         for i in reversed(range(len(self.layers))):
             next_layers_gradient, theta_i_grad, original_theta_i =\
-                self.layers[i].backward(next_layers_gradient, grad_test)
+                self.layers[i].backward(next_layers_gradient, jac_test)
             all_theta_grad = np.concatenate((all_theta_grad, theta_i_grad), axis=0)
             all_theta_orig = np.concatenate((all_theta_orig, original_theta_i), axis=0)
         return all_theta_orig, all_theta_grad
 
-    def train(self, learning_rate, data_matrix, y_true, grad_test):
+    def train(self, learning_rate, data_matrix, y_true, grad_test, jac_test):
         y_pred = self.feedforward(data_matrix)
         success_percentage = self.success_percentage(y_true, y_pred)
         loss = self.loss_layer.get_loss(y_true)
         dx_loss, d_theta, original_theta_loss = self.loss_layer.calculate_gradients(y_true, grad_test)
-        all_theta_old, all_theta_grad = self.backpropagation(d_theta, original_theta_loss, dx_loss, grad_test)
+        all_theta_old, all_theta_grad = self.backpropagation(d_theta, original_theta_loss, dx_loss, jac_test)
         theta_new = self.sgd_update_theta(learning_rate, all_theta_old, all_theta_grad)
         self.update_theta_layers(theta_new)
-        return success_percentage, loss, y_pred
+        return success_percentage, loss
 
     def test(self, data_matrix, y_true):
         y_pred = self.feedforward(data_matrix)

ResLayer.py
@@ -9,10 +9,8 @@ class ResLayer:
         :param mid_dim: number of neurons in the activation calculation
         :param input_output_dim: number of neurons in input and output
         :param activation_name: name of activation function of this layer
-        weights_1: (mid_dim * input_output_dim) weights
-        bias_1: (mid_dim * 1) vector
-        weights_2: (input_output_dim * mid_dim) weights
-        bias_2: (input_output_dim * 1) vector
+        weights: (mid_dim * input_output_dim) weights
+        bias: (mid_dim * 1) vector
         """
         self.weights_1 = np.random.uniform(-0.5, 0.5, (mid_dim, input_output_dim))
         self.weights_2 = np.random.uniform(-0.5, 0.5, (input_output_dim, mid_dim))